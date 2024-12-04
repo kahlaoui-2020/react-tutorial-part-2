beec640cfb0be32803e58334b00b832d0dd5034c
kahlaoui-2020 <61467536+kahlaoui-2020@users.noreply.github.com>
2024-12-04 13:58:05 +0000

    profile class component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,79 @@
 import { Component } from "react";
 import { ThemeContext } from "../utils/context/index";
 import styled from "styled-components";
-const ProfileWrapper = styled.div``;
-const Picture = styled.img``;
-const ProfileDetail = styled.div``;
-const TitleWrapper = styled.div``;
-const Title = styled.h1``;
-const Location = styled.span``;
-const JobTitle = styled.h2``;
-const SkillsWrapper = styled.div``;
-const Skill = styled.span``;
-const Availability = styled.span``;
-const Price = styled.span``;
+import colors from "../utils/style/colors";
+const ProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 90px 0;
+  margin: 0 90px;
+  background-color: ${({ theme }) =>
+    theme === "light" ? colors.backgroundLight : colors.backgroundDark};
+`;
+const Picture = styled.img`
+  height: 150px;
+  width: 150px;
+  border-radius: 75px;
+`;
+const ProfileDetail = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 50px;
+  color: ${({ theme }) => (theme === "light" ? colors.dark : colors.light)};
+`;
+const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 25px;
+  margin: 0;
+  font-weight: 500;
+`;
+const Location = styled.span`
+  margin-left: 15px;
+  color: ${colors.secondary};
+`;
+const JobTitle = styled.h2`
+  padding-top: 10px;
+  font-size: 20px;
+  margin: 0;
+  font-weight: 500;
+`;
+const SkillsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 0;
+`;
+const Skill = styled.span`
+  border-radius: 5px;
+  padding: 5px;
+  margin-right: 5px;
+  border: 1px solid
+    ${({ theme }) => (theme === "light" ? colors.dark : colors.light)};
+`;
+const Availability = styled.span`
+  :before {
+    position: absolute;
+    left: 0;
+    top: 4px;
+    height: 10px;
+    width: 10px;
+    border-radius: 5px;
+    background-color: ${({ available }) => (available ? "green" : "red")};
+    content: "";
+  }
+  padding-left: 20px;
+  position: relative;
+`;
+const Price = styled.span`
+  padding-top: 10px;
+  font-weight: 500;
+  font-size: 20px;
+`;
 class Profile extends Component {
   constructor(props) {
     super(props);
@@ -37,30 +99,28 @@ class Profile extends Component {
     return (
       <ThemeContext.Consumer>
         {({ theme }) => (
-          <div theme={theme} className="ProfileWrapper">
-            <img src={picture} className="Picture" />
-            <div theme={theme} className="ProfileDetail">
-              <div className="TitleWrapper">
-                <h1 className="Title"></h1>
-                <span className="Location"></span>
-              </div>
-              <h2 className="JobTitle"></h2>
-              <div className="SkillsWrapper">
+          <ProfileWrapper theme={theme}>
+            <Picture src={picture} />
+            <ProfileDetail theme={theme}>
+              <TitleWrapper>
+                <Title>{name}</Title>
+                <Location>{loacation}</Location>
+              </TitleWrapper>
+              <JobTitle>{job}</JobTitle>
+              <SkillsWrapper>
                 {skills &&
                   skills.map((skill) => (
-                    <span
-                      key={`skill-${id}-${skill}`}
-                      className="Skill"
-                      theme={theme}
-                    >
+                    <Skill key={`skill-${id}-${skill}`} theme={theme}>
                       {skill}
-                    </span>
+                    </Skill>
                   ))}
-              </div>
-              <span className="Availability"></span>
-              <span className="Price"></span>
-            </div>
-          </div>
+              </SkillsWrapper>
+              <Availability available={available}>
+                {available ? "Disponible" : "Indisponible"}
+              </Availability>
+              <Price>{tjm}€/jour</Price>
+            </ProfileDetail>
+          </ProfileWrapper>
         )}
       </ThemeContext.Consumer>
     );
